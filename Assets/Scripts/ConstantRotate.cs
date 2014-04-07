7123982ef26824a2fda21baa05ffb080815e9e42
Kevin Alford <zeroeth@pixelflow.org>
2014-04-06 19:29:16 -0500

    Game files, 1 player crazy thrust physics.

---
+++ Assets/Scripts/ConstantRotate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotate : MonoBehaviour {
+
+	public float rotation_speed = 35.0f;
+
+	// Use this for initialization
+	void Start () {
+		transform.rotation = Quaternion.AngleAxis(Random.value * 360.0f, Vector3.forward);
+		rotation_speed = (Random.value - 0.5f) * rotation_speed * 2.0f;
+
+		//rigidbody2D.AddTorque(rotation_speed);
+		rigidbody2D.angularVelocity = rotation_speed;
+		rigidbody2D.AddForce (transform.up * (Random.Range(5.0f, 100.0f)));
+	}
+	
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rigidbody2D.angularVelocity = rotation_speed;
+	}
+
+
+	// Collide
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			//Destroy( col.gameObject );
+		}
+		else if (col.gameObject.tag == "Asteroid") {
+			// Split if velocity high enough
+		}
+	}
+}
---
+++ Assets/Scripts/MoveAround.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAround : MonoBehaviour {
+
+	public float rotation_speed = 0.5f;
+	public float thrust_speed = 5.0f;
+	public float reverse_speed = 0.1f;
+	public GameObject projectile;
+
+    public float fire_rate = 0.5f;
+    private float next_fire = 0.0f;
+
+	public float bullet_velocity = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Rotate
+		float horizontal = -Input.GetAxis ("Horizontal");
+		rigidbody2D.AddTorque (horizontal * rotation_speed);
+
+
+		// Thrust
+		if (Input.GetAxis ("Vertical") > 0) {
+			rigidbody2D.AddForce (transform.up * thrust_speed);
+		}
+
+		if (Input.GetAxis ("Vertical") < 0) {
+			rigidbody2D.AddForce (-transform.up * thrust_speed * reverse_speed);
+		}
+
+
+		// Fire
+		if (Input.GetButton("Jump") && Time.time > next_fire) {
+            next_fire = Time.time + fire_rate;
+			GameObject beam = (GameObject) Instantiate(projectile, transform.position, transform.rotation);
+			
+			// Combine base velocity with parent velocity
+			beam.rigidbody2D.velocity = rigidbody2D.velocity + (Vector2)transform.up * bullet_velocity;
+		}
+	}
+}
